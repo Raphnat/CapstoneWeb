1bf816b9ef20954642a09b2efae1bbde0dc3ed0b
Annette <S10226123@connect.np.edu.sg>
2023-05-22 04:39:10 +0800

    Update firebaseAuth.js

--- firebaseAuth.js
+++ firebaseAuth.js
@@ -13,6 +13,7 @@ const firebaseConfig = {
   appId: "1:431465307758:web:bb5b2cf610e7facd1a507f"
 };
 
+//AUTHENTICATION STUFF
 // Initialize Firebase
 const app = initializeApp(firebaseConfig); 
 
@@ -23,25 +24,36 @@ function signUp(username, email, password,cfmPassword, hawkerCode) {
       } 
 
       // additional: how to ensure hawker code is legit?
-    firebase.auth().createUserWithEmailAndPassword(username, email, password, hawkerCode)
+      firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
-        
         const user = userCredential.user;
         console.log(`${user.uid} signed up successfully ${user.email}`);
   
-        // custom claim
-        return user.getIdToken().then((idToken) => {
-          return fetch('/setCustomClaims', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ idToken: idToken })
+        // Set custom claim 
+        const customClaims = {
+          hawkerCode: hawkerCode,
+          type: "Hawker"
+        };
+  
+        return user.getIdToken()
+          .then((idToken) => {
+            return fetch('/setCustomClaims', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ idToken: idToken, customClaims: customClaims })
+            });
+          })
+          .then(() => {
+            console.log("Custom claim set successfully!");
+          })
+          .catch((error) => {
+            console.error("Error setting custom claim: " + error);
           });
-        });
       })
       .catch((error) => {
-        console.error(`error signing up: ${error.message}`);
+        console.error(`Error signing up: ${error.message}`);
       });
   }
   function logIn(email, password) {
@@ -87,3 +99,86 @@ setCustomClaims({ type: 'Hawker' })
   .catch((error) => {
     console.error(`Error setting cc: ${error.message}`);
   });
+
+  //DATABASE STUFF
+  //MENU PART
+  function menuAccess(centerName, stallName) {
+
+    var menuRef = firebase.database().ref(centerName + '/'+ stallName + '/menu');
+  
+
+    menuRef.once('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var itemKey = childSnapshot.key;
+        var itemData = childSnapshot.val();
+
+        console.log("Item key: " + itemKey);
+        console.log("Item name: " + itemData.name);
+        console.log("Item price: " + itemData.price);
+
+      });
+    });
+  }
+
+  function addItem(centerName, stallName, itemName, itemPrice) {
+    var menuRef = firebase.database().ref(centerName + '/'+ stallName + '/menu');
+    
+    var newItemRef = menuRef.push();
+    newItemRef.set({
+      name: itemName,
+      price: itemPrice
+    })
+      .then(function() {
+        console.log("Item added");
+      })
+      .catch(function(error) {
+        console.error("Error adding item: " + error);
+      });
+  }
+
+  function deleteItem(centerName, stallName, itemName) {
+    var menuRef = firebase.database().ref(centerName + '/'+ stallName + '/menu');
+  
+    menuRef.orderByChild('name').equalTo(itemName).once('value', function(snapshot) {
+      if (snapshot.exists()) {
+        snapshot.forEach(function(childSnapshot) {
+          var itemKey = childSnapshot.key;
+          var itemRef = catalogRef.child(itemKey);
+          itemRef.remove()
+            .then(function() {
+
+              console.log("Item deleted:"+ itemName);
+
+            })
+            .catch(function(error) {
+              console.error("Error deleting item " + error);
+            });
+        });
+      } else {
+        console.log(itemName + "does not exist");
+      }
+    });
+  }
+  function updatePrice(centerName, stallName, itemName, newItemPrice) {
+    var menuRef = firebase.database().ref(centerName + '/' + stallName + '/menu');
+  
+    menuRef.orderByChild('name').equalTo(itemName).once('value', function(snapshot) {
+      if (snapshot.exists()) {
+        snapshot.forEach(function(childSnapshot) {
+          var itemKey = childSnapshot.key;
+          catalogRef.child(itemKey).update({ price: newItemPrice })
+            .then(function() {
+
+              console.log("Updated item: " + itemName);
+              console.log("New price: " + newItemPrice);
+
+            })
+            .catch(function(error) {
+              console.error("Error updating price: " + error);
+            });
+        });
+      } else {
+        console.log(itemName + "does not exist");
+      }
+    });
+  }
